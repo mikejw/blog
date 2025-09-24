4fbfa1d626adb6c3e14d3ec235f242fab5db4cc0
Mike Whiting <mikejw3@gmail.com>
2025-09-24 21:03:35 +0100

    site info fix

--- application/about/about.php
+++ application/about/about.php
@@ -4,6 +4,11 @@ namespace Empathy\MVC\Controller;
 
 class about extends \Empathy\ELib\Blog\BlogFrontControllerNew
 {
+    public function __construct($boot)
+    {
+        parent::__construct($boot);
+    }
+    
     public function default_event()
     {
         //
