bd9900c4609eb51039aa5ccdcf098efcf79a770a
Mike Whiting <mail@mikejw.co.uk>
2025-12-15 19:05:18 +0000

    first pass (unsuccessful) of state-less category selection

--- application/blog/blog.php
+++ application/blog/blog.php
@@ -2,7 +2,29 @@
 
 namespace Empathy\MVC\Controller;
 
+use Empathy\MVC\Session;
+
 class blog extends \Empathy\ELib\Blog\BlogFrontControllerNew
 {
     //
+    // @TODO - needs more work
+    // with this approach categories are forgotten
+    // when navigating tags
+
+    //     public function category(): void
+    //     {
+    //         $cat = $_GET['category'];
+    //         $cache = $this->stash->get('cache');
+    //
+    //         $cat_id = $cache->cachedCallback('category_'.$cat,
+    //         array($this, 'fetchCategoryId'), array($cat));
+    //
+    //         // do not do this!
+    //         //Session::set('blog_category', $cat_id);
+    //
+    //         $this->stash->store('blog_category', $cat_id);
+    //         $this->assign('blog_category', $cat_id);
+    //
+    //         $this->default_event();
+    //     }
 }
